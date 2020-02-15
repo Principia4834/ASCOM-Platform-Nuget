b2f0b5168a4b8df1822551053f4e4cb6fc58dbb4
Tim Long <Tim@tigranetworks.co.uk>
2020-02-15 16:12:20 +0000

    Added assemblies from platform 6.5 RC1 and updated the project to be more "public facing"

--- Build-Package.ps1
+++ Build-Package.ps1
@@ -1,2 +1,8 @@
+$gitVersion = GitVersion.exe | ConvertFrom-Json
+$semver = $gitVersion.SemVer
+$majorVersion = $gitVersion.Major
+$minorVersion = $gitVersion.Minor
+$nugetVersion = $gitVersion.NuGetVersion
+
 New-Item -ErrorAction Ignore -ItemType directory -Path Nuget-Packages
-nuget pack .\root\ASCOM.Platform.nuspec -OutputDirectory .\Nuget-Packages
+nuget pack .\root\ASCOM.Platform.nuspec -OutputDirectory .\Nuget-Packages -Version $nugetVersion
